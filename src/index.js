f19241cdf3ad99e0f8da4400f3e7677557dc1451
Fernando De Leon <gasnare@hotmail.com>
2023-08-14 20:53:15 -0500

    se agregan imagenes

--- src/index.js
+++ src/index.js
@@ -3,4 +3,30 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+{/* <div class="p-4">
+    <img class="mx-auto" width="320" src="https://randomfox.ca/images/3.jpg" alt="">
+</div> */}
+
+const minium = 1;
+const maxium = 122;
+const random = Math.floor(Math.random() * (maxium - minium) + minium);
+
+const createImageNode = () => {
+    const container = document.createElement('div');
+    container.className = 'p-4';
+
+    const image = document.createElement('img');
+    image.className = 'mx-auto';
+    image.width = '320';
+    image.src =`https://randomfox.ca/images/${random}.jpg`;
+
+    container.appendChild(image);
+
+    return container;
+}
+
+const newImage = createImageNode();
+
+const mountNode = document.getElementById('images');
+
+mountNode.appendChild(newImage);
